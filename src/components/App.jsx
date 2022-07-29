1dc84a25359c6dd99faa87320b0c24780bba8b40
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-07-29 20:38:19 +0100

    Added folders and files in components and starter information about form contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import { ContactForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = event => {
+    // console.log(event.target);
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+
+    // event.preventDefault();
+    // console.log(event.target);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </form>
+        {/* <ContactForm
+          value={this.state.name}
+          // buttonName="Add contact"
+          onChange={this.handleInputValue}
+        /> */}
+      </>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,31 @@
+// import React, { Component } from 'react';
+
+// export class ContactForm extends Component {
+
+// state = {
+//     name: "",
+//     id: "",
+// }
+//   render() {
+//     const { value, buttonName, onChange } = this.props;
+
+//     return (
+//       <form>
+//         <label>
+//           Name{' '}
+//           <input
+//             type="text"
+//             name="name"
+//             value={value}
+//             onChange={onChange}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//           />
+//         </label>
+
+//         {/* <button type="submit">{buttonName}</button> */}
+//       </form>
+//     );
+//   }
+// }
