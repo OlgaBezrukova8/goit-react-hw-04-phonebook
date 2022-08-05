b02e126eeff875369d03ea5118be17aaf792dbb9
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-08-05 16:59:56 +0100

    Code refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,8 +20,8 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState) {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
